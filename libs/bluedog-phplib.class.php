f3950bcd18103bf471a860250ce991ac1365684a
Andrew <andrew@nomstock.com>
2016-08-02 18:41:14 -0700

    json configuration conversion, command line switches to generate samples and help

--- libs/bluedog-phplib.class.php
+++ libs/bluedog-phplib.class.php
@@ -240,7 +240,89 @@ class bluedog_phplib {
             return;
         }
     }
-} 
+}
+
+    /**
+     * Pretty Prints Json
+     *
+     * returns json object formatted for heirarchical html output
+     * ref: http://stackoverflow.com/a/9776726/3306354
+     * 
+     * @param string $json  A json object
+     * @return void
+     */
+    
+    public function JsonPrettyPrint( $json )
+{
+    $result = '';
+    $level = 0;
+    $in_quotes = false;
+    $in_escape = false;
+    $ends_line_level = NULL;
+    $json_length = strlen( $json );
+
+    for( $i = 0; $i < $json_length; $i++ ) {
+        $char = $json[$i];
+        $new_line_level = NULL;
+        $post = "";
+        if( $ends_line_level !== NULL ) {
+            $new_line_level = $ends_line_level;
+            $ends_line_level = NULL;
+        }
+        if ( $in_escape ) {
+            $in_escape = false;
+        } else if( $char === '"' ) {
+            $in_quotes = !$in_quotes;
+        } else if( ! $in_quotes ) {
+            switch( $char ) {
+                case '}': case ']':
+                    $level--;
+                    $ends_line_level = NULL;
+                    $new_line_level = $level;
+                    break;
+
+                case '{': case '[':
+                    $level++;
+                case ',':
+                    $ends_line_level = $level;
+                    break;
+
+                case ':':
+                    $post = " ";
+                    break;
+
+                case " ": case "\t": case "\n": case "\r":
+                    $char = "";
+                    $ends_line_level = $new_line_level;
+                    $new_line_level = NULL;
+                    break;
+            }
+        } else if ( $char === '\\' ) {
+            $in_escape = true;
+        }
+        if( $new_line_level !== NULL ) {
+            $result .= "\n".str_repeat( "\t", $new_line_level );
+        }
+        $result .= $char.$post;
+    }
+
+    return $result;
+}
+
+
+    /**
+     * isCommandLine
+     *
+     * Checks whether script is being called by command line interface or web request
+     *
+     * @param none
+     * @return boolean
+     */
+    public function isCommandLine() {
+
+        return (php_sapi_name() === 'cli');
+    }
+    
 
 }
 
